a743b505c38dc5b01f34159b129d94a47e710352
Mithat <mithatsirmali@gmail.com>
2025-11-17 13:59:51 +0300

    First scripts preparations

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+    }
+}
